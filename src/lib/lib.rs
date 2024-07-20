68b751050e2d94921c555b2ed3c76a030406478e
Tiago Schmidt <68293535+tiagotschmidt@users.noreply.github.com>
2024-07-20 12:44:18 -0300

    Adding long term registry.

---
+++ src/lib/lib.rs
@@ -0,0 +1,5 @@
+pub mod delta_hours;
+pub mod long_term_registry;
+pub mod work_days;
+pub mod work_journey;
+pub mod work_week;
---
+++ src/lib/long_term_registry.rs
@@ -0,0 +1,52 @@
+use std::num::TryFromIntError;
+
+use chrono::TimeDelta;
+use thiserror::Error;
+
+use crate::{delta_hours::DeltaHours, work_week::WorkWeek};
+
+#[derive(Error, Debug)]
+pub enum LongTermRegistryError {
+    #[error("Empty history.")]
+    EmptyHistory,
+    #[error("Error during int conversion {0}")]
+    IntConversionError(TryFromIntError),
+}
+
+struct LongTermRegistry {
+    history: Vec<WorkWeek>,
+}
+
+impl LongTermRegistry {
+    fn new() -> LongTermRegistry {
+        let history = Vec::new();
+
+        LongTermRegistry { history }
+    }
+
+    pub fn worked_hours(&self) -> TimeDelta {
+        self.history
+            .iter()
+            .fold(TimeDelta::zero(), |mut acc, item| {
+                acc += item.worked_hours();
+                acc
+            })
+    }
+
+    pub fn worked_delta(&self) -> Result<DeltaHours, LongTermRegistryError> {
+        let mut current_delta = self
+            .history
+            .first()
+            .ok_or(LongTermRegistryError::EmptyHistory)?
+            .worked_delta()
+            .map_err(LongTermRegistryError::IntConversionError)?;
+
+        for week in self.history.iter().skip(1) {
+            current_delta += week
+                .worked_delta()
+                .map_err(LongTermRegistryError::IntConversionError)?
+        }
+
+        Ok(current_delta)
+    }
+}
